3d07eec6eafe79213cb71eb19b00f1c5d8c6acfa
garrett <garrettshields@protonmail.com>
2019-07-11 00:35:07 -0400

    * Organized / Cleaned up Flipbook classes.

--- Flipbook/Flipbook.cs
+++ Flipbook/Flipbook.cs
@@ -6,88 +6,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace Gruel.Flipbook {
-	// [ExecuteInEditMode]
 	public class Flipbook : MonoBehaviour, IPoolable {
-
-#region Init
-		private void Start() {
-			FlipbookStart();
-		}
-
-		private void OnDestroy() {
-			FlipbookOnDestroy();
-		}
-#endregion Init
-	
-#region Poolable
-		[Header("Poolable")]
-		[SerializeField] private bool _poolWhenFinished = false;
 		
-		private int _hash;
-	
-		public void Pool() {
-			// Reset flipbook.
-			FlipbookPool();
-		
-			// Disable our GameObject.
-			gameObject.SetActive(false);
-			
-			// Reset transform.
-			transform.rotation = Quaternion.identity;
-			transform.localScale = Vector3.one;
-		}
-	
-		public void Unpool() {
-			// Listen for when flipbook finishes playing.
-			_onFinishedPlaying += PoolableOnFinishedPlaying;
-			
-			// Enable our GameObject.
-			gameObject.SetActive(true);
-		}
-
-		public int GetHash() {
-			return _hash;
-		}
-	
-		public void SetHash(int hash) {
-			_hash = hash;
-		}
-
-		public void Destroy() {
-			if (gameObject != null) {
-				GameObject.Destroy(gameObject);
-			}
-		}
-		
-		private void PoolableOnFinishedPlaying() {
-			if (_poolWhenFinished) {
-				ObjectPool.ObjectPool.Repool(this);
-			}
-		}
-#endregion Poolable
-
-#region Flipbook
-		[Header("Flipbook")]
-		[SerializeField] private FlipbookData _flipbookData;
-		[SerializeField] private bool _playOnStart = true;
-		public bool _startAtRandomPlaybackPosition;
-		public float _delay;
-		public bool _clearLastFrame;
-
-		[Header("Renderer")]
-		[SerializeField] private SpriteRenderer _spriteRenderer;
-		[SerializeField] private Image _image;
-
-		private ManagedCoroutine _flipbookCor;
+#region Properties
 		public bool Playing { get; private set; }
-	
-		private bool _loop = true;
-		private int _numberOfFrames;
-		private float _frameDelay;
-		private float _duration;
-
-		public Action _onFinishedPlaying;
-	
+		public Action OnFinishedPlaying;
+		
 		public Color _tint {
 			get {
 				if (_spriteRenderer != null) {
@@ -113,15 +37,35 @@ namespace Gruel.Flipbook {
 				}
 			}
 		}
+#endregion Properties
 
-		private void FlipbookStart() {
-			// If _playOnStart is enabled start playing.
-			if (_playOnStart) {
-				Play(true);
-			}
-		}
+#region Fields
+		[Header("Flipbook")]
+		[SerializeField] private FlipbookData _flipbookData;
+		[SerializeField] private bool _playOnStart = true;
+		public bool _startAtRandomPlaybackPosition;
+		public float _delay;
+		public bool _clearLastFrame;
 
-		private void FlipbookPool() {
+		[Header("Renderer")]
+		[SerializeField] private SpriteRenderer _spriteRenderer;
+		[SerializeField] private Image _image;
+
+		[Header("Poolable")]
+		[SerializeField] private bool _poolWhenFinished;
+
+		private ManagedCoroutine _flipbookCor;
+		
+		private bool _loop = true;
+		private int _numberOfFrames;
+		private float _frameDelay;
+		private float _duration;
+		
+		private int _hash;
+#endregion Fields
+
+#region Public Methods
+		public void Pool() {
 			if (Playing) {
 				Play(false);
 			}
@@ -130,16 +74,40 @@ namespace Gruel.Flipbook {
 			_delay = 0.0f;
 			_spriteRenderer.color = Color.white;
 
-			_onFinishedPlaying = null;
+			OnFinishedPlaying = null;
+		
+			// Disable our GameObject.
+			gameObject.SetActive(false);
+			
+			// Reset transform.
+			transform.rotation = Quaternion.identity;
+			transform.localScale = Vector3.one;
+		}
+	
+		public void Unpool() {
+			// Listen for when flipbook finishes playing.
+			OnFinishedPlaying += PoolableOnFinishedPlaying;
+			
+			// Enable our GameObject.
+			gameObject.SetActive(true);
 		}
 
-		private void FlipbookOnDestroy() {
-			// Stop playing.
-			Play(false);
+		public int GetHash() {
+			return _hash;
+		}
+	
+		public void SetHash(int hash) {
+			_hash = hash;
 		}
 
+		public void Destroy() {
+			if (gameObject != null) {
+				GameObject.Destroy(gameObject);
+			}
+		}
+		
 		public void Play(FlipbookData flipbookData) {
-			this._flipbookData = flipbookData;
+			_flipbookData = flipbookData;
 			Play(true);
 		}
 	
@@ -162,24 +130,44 @@ namespace Gruel.Flipbook {
 		
 			if (play) {
 				// Cache data.
-				_frameDelay = 1.0f / _flipbookData._framesPerSecond;
-				_numberOfFrames = _flipbookData._keyFrames.Length;
+				_frameDelay = 1.0f / _flipbookData.FramesPerSecond;
+				_numberOfFrames = _flipbookData.Length;
 				_duration = _frameDelay * _numberOfFrames;
-				_loop = _flipbookData._loop;
+				_loop = _flipbookData.Loop;
 			
 				if (_spriteRenderer != null) {
-					_spriteRenderer.material = _flipbookData._material;
+					_spriteRenderer.material = _flipbookData.Material;
 				}
 
 				if (_image != null) {
-					_image.material = _flipbookData._material;
+					_image.material = _flipbookData.Material;
 				}
 
 				Playing = true;
 				_flipbookCor = CoroutineRunner.StartManagedCoroutine(FlipbookCor());
 			}
 		}
+#endregion Public Methods
 
+#region Private Methods
+		private void Start() {
+			// If _playOnStart is enabled start playing.
+			if (_playOnStart) {
+				Play(true);
+			}
+		}
+
+		private void OnDestroy() {
+			// Stop playing.
+			Play(false);
+		}
+		
+		private void PoolableOnFinishedPlaying() {
+			if (_poolWhenFinished) {
+				ObjectPool.ObjectPool.Repool(this);
+			}
+		}
+		
 		private IEnumerator FlipbookCor() {
 			if (_delay > 0.0f) {
 				yield return new WaitForSeconds(_delay);
@@ -206,24 +194,24 @@ namespace Gruel.Flipbook {
 						}
 					}
 				
-					_onFinishedPlaying?.Invoke();
+					OnFinishedPlaying?.Invoke();
 					break;
 				}
 				
 				var frame = (int)Mathf.Lerp(0.0f, _numberOfFrames, playbackTimeNormal);
 			
 				if (_spriteRenderer != null) {
-					_spriteRenderer.sprite = _flipbookData._keyFrames[frame];
+					_spriteRenderer.sprite = _flipbookData[frame];
 				}
 
 				if (_image != null) {
-					_image.sprite = _flipbookData._keyFrames[frame];
+					_image.sprite = _flipbookData[frame];
 				}
 				
 				yield return null;
 			}
 		}
-#endregion Flipbook
+#endregion Private Methods
 
 	}
 }
--- Flipbook/FlipbookData.cs
+++ Flipbook/FlipbookData.cs
@@ -4,11 +4,31 @@ namespace Gruel.Flipbook {
 	[CreateAssetMenu(menuName = "Gruel/FlipbookData")]
 	public class FlipbookData : ScriptableObject {
 
-		public bool _loop = true;
-		public Material _material;
+#region Properties
+		public bool Loop {
+			get => _loop;
+		}
+
+		public Material Material {
+			get => _material;
+		}
+
+		public int FramesPerSecond {
+			get => _framesPerSecond;
+		}
+		
+		public Sprite this[int index] => _keyFrames[index];
+
+		public int Length => _keyFrames.Length;
+#endregion Properties
+
+#region Fields
+		[SerializeField] private bool _loop = true;
+		[SerializeField] private Material _material;
 	
-		public int _framesPerSecond = 30;
-		public Sprite[] _keyFrames = new Sprite[0];
+		[SerializeField] private int _framesPerSecond = 30;
+		[SerializeField] private Sprite[] _keyFrames = new Sprite[0];
+#endregion Fields
 
 	}
 }
